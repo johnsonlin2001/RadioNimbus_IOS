7cb8a1c5f87e4e8c545b7728ea580e249c158d9a
Johnson Lin <yongchen@ucsb.edu>
2024-11-25 23:42:46 -0800

    initialy styling to today tab

--- RadioNimbus/TodayTabController.swift
+++ RadioNimbus/TodayTabController.swift
@@ -13,13 +13,28 @@ class TodayTabController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        sub1.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        sub2.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        sub3.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        sub1.layer.borderColor = UIColor.white.cgColor
+        sub1.layer.borderWidth = 1.0
+        sub2.layer.borderColor = UIColor.white.cgColor
+        sub2.layer.borderWidth = 1.0
+        sub3.layer.borderColor = UIColor.white.cgColor
+        sub3.layer.borderWidth = 1.0
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.navigationController?.setNavigationBarHidden(false, animated: true)
     }
     
-
+    
+    @IBOutlet weak var sub1: UIView!
+    
+    @IBOutlet weak var sub2: UIView!
+    
+    @IBOutlet weak var sub3: UIView!
+    
     /*
     // MARK: - Navigation
 

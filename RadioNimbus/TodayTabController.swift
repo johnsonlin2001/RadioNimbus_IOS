07a9c9ee33b413c27e314a418462dfcbc2fd563d
Johnson Lin <yongchen@ucsb.edu>
2024-11-26 15:22:13 -0800

    completed layout and cosmetics for today tab

--- RadioNimbus/TodayTabController.swift
+++ RadioNimbus/TodayTabController.swift
@@ -16,12 +16,36 @@ class TodayTabController: UIViewController {
         sub1.backgroundColor = UIColor.white.withAlphaComponent(0.5)
         sub2.backgroundColor = UIColor.white.withAlphaComponent(0.5)
         sub3.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        sub4.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        sub5.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        sub6.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        sub7.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        sub8.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        sub9.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        
         sub1.layer.borderColor = UIColor.white.cgColor
         sub1.layer.borderWidth = 1.0
         sub2.layer.borderColor = UIColor.white.cgColor
         sub2.layer.borderWidth = 1.0
         sub3.layer.borderColor = UIColor.white.cgColor
         sub3.layer.borderWidth = 1.0
+        
+        sub4.layer.borderColor = UIColor.white.cgColor
+        sub4.layer.borderWidth = 1.0
+        sub5.layer.borderColor = UIColor.white.cgColor
+        sub5.layer.borderWidth = 1.0
+        sub6.layer.borderColor = UIColor.white.cgColor
+        sub6.layer.borderWidth = 1.0
+        
+        sub7.layer.borderColor = UIColor.white.cgColor
+        sub7.layer.borderWidth = 1.0
+        sub8.layer.borderColor = UIColor.white.cgColor
+        sub8.layer.borderWidth = 1.0
+        sub9.layer.borderColor = UIColor.white.cgColor
+        sub9.layer.borderWidth = 1.0
+        
+        self.tabBarController?.tabBar.barTintColor = .white
+        self.tabBarController?.tabBar.isTranslucent = false
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -34,6 +58,19 @@ class TodayTabController: UIViewController {
     @IBOutlet weak var sub2: UIView!
     
     @IBOutlet weak var sub3: UIView!
+    
+    @IBOutlet weak var sub4: UIView!
+    
+    @IBOutlet weak var sub5: UIView!
+    
+    @IBOutlet weak var sub6: UIView!
+    
+    @IBOutlet weak var sub7: UIView!
+    
+    @IBOutlet weak var sub8: UIView!
+    
+    @IBOutlet weak var sub9: UIView!
+    
     
     /*
     // MARK: - Navigation

9ba7503862aff9aa204ed57fd0b8a896aa19f73f
Johnson Lin <yongchen@ucsb.edu>
2024-12-06 15:50:19 -0800

    initial chart positioning implemented

--- RadioNimbus/weeklyTabController.swift
+++ RadioNimbus/weeklyTabController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Highcharts
 
 class weeklyTabController: UIViewController {
     var currentTemp: Int?
@@ -15,6 +16,9 @@ class weeklyTabController: UIViewController {
     @IBOutlet weak var statusLabel: UILabel!
     
     @IBOutlet weak var tempLabel: UILabel!
+    
+    
+    @IBOutlet weak var chartSub: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -28,6 +32,80 @@ class weeklyTabController: UIViewController {
         statusLabel.text = currentStatus
         tempLabel.text = "\((currentTemp) ?? 0)°F"
         statusImg.image = UIImage(named: currentStatus ?? "Clear")
+        
+        let chartView = HIChartView(frame: self.view.bounds)
+        chartView.plugins = ["exporting", "series-label", "accessibility"]
+
+        // Chart Configuration
+        let options = HIOptions()
+        
+        // Chart type and settings
+        let chart = HIChart()
+        chart.type = "arearange"
+        chart.zooming = HIZooming()
+        chart.zooming.type = "x"
+        chart.scrollablePlotArea = nil
+        options.chart = chart
+        
+        // Chart title
+        let title = HITitle()
+        title.text = "Temperature Ranges (Min, Max)"
+        options.title = title
+        
+        // X-Axis configuration
+        let xAxis = HIXAxis()
+        xAxis.type = "datetime"
+        let accessibility = HIAccessibility()
+        accessibility.rangeDescription = "Date range of the weather data."
+        xAxis.accessibility = accessibility
+        options.xAxis = [xAxis]
+        
+        // Y-Axis configuration
+        let yAxis = HIYAxis()
+        yAxis.title = HITitle()
+        yAxis.title.text = "Temperature (°F)"
+        options.yAxis = [yAxis]
+        
+        // Tooltip configuration
+        let tooltip = HITooltip()
+        tooltip.shared = NSNumber(value: true)
+        tooltip.valueSuffix = "°F"
+        tooltip.xDateFormat = "%A, %b %e"
+        options.tooltip = tooltip
+        
+        // Disable legend
+        let legend = HILegend()
+        legend.enabled = NSNumber(value: false)
+        options.legend = legend
+        
+        // Series configuration
+        let series = HIArearange()
+        series.name = "Temperatures"
+        series.data = [
+            [1668864000000, 52, 62],  // Example data: [timestamp, minTemp, maxTemp]
+            [1668950400000, 54, 64],
+            [1669036800000, 50, 60]
+        ]
+        series.color = HIColor()
+        options.series = [series]
+        
+        // Assign options to the chart view
+        chartView.options = options
+        
+        chartView.translatesAutoresizingMaskIntoConstraints = false
+
+        chartSub.addSubview(chartView)
+
+        // Set constraints to make the chartView fit the container view
+        NSLayoutConstraint.activate([
+            chartView.leadingAnchor.constraint(equalTo: chartSub.leadingAnchor),
+            chartView.trailingAnchor.constraint(equalTo: chartSub.trailingAnchor),
+            chartView.topAnchor.constraint(equalTo: chartSub.topAnchor),
+            chartView.bottomAnchor.constraint(equalTo: chartSub.bottomAnchor)
+        ])
+        
+        // Add chartView to the view
+        chartSub.addSubview(chartView)
     }
     
     @IBOutlet weak var topview: UIView!

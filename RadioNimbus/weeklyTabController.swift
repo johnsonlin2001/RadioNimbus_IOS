655325db969ea7a49bb3119a8cc22f06b3aebe21
Johnson Lin <yongchen@ucsb.edu>
2024-12-05 23:41:13 -0800

    added controller for weekly tab

---
+++ RadioNimbus/weeklyTabController.swift
@@ -0,0 +1,31 @@
+//
+//  weeklyTabController.swift
+//  RadioNimbus
+//
+//  Created by Johnson Lin on 2024/12/5.
+//
+
+import UIKit
+
+class weeklyTabController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.tabBarController?.tabBar.barTintColor = .white
+        self.tabBarController?.tabBar.isTranslucent = false
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

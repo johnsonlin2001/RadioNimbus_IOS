87e14222660715e5d486a55b095b4f63ed05e414
Johnson Lin <yongchen@ucsb.edu>
2024-12-06 21:57:15 -0800

    code cleanup

--- RadioNimbus/weeklyTabController.swift
+++ RadioNimbus/weeklyTabController.swift
@@ -31,16 +31,13 @@ class weeklyTabController: UIViewController {
                let values = day["values"] as? [String: Any],
                let tempLow = values["temperatureMin"] as? Double,
                let tempHigh = values["temperatureMax"] as? Double {
-                
-                // Convert startTime to Date
-                let inputDateFormatter = ISO8601DateFormatter()
-                if let date = inputDateFormatter.date(from: startTime) {
+               let inputDateFormatter = ISO8601DateFormatter()
+               if let date = inputDateFormatter.date(from: startTime) {
                     // Convert date to UTC timestamp
                     let calendar = Calendar(identifier: .gregorian)
                     let components = calendar.dateComponents([.year, .month, .day], from: date)
                     if let utcDate = calendar.date(from: components) {
-                        let timestamp = utcDate.timeIntervalSince1970 * 1000 // Convert to milliseconds
-                        // Append the processed data
+                        let timestamp = utcDate.timeIntervalSince1970 * 1000
                         temperatureData.append([timestamp, tempLow, tempHigh])
                     }
                 }
@@ -61,71 +58,68 @@ class weeklyTabController: UIViewController {
         let chartView = HIChartView(frame: self.view.bounds)
         chartView.plugins = ["exporting", "series-label", "accessibility"]
 
-        // Chart Configuration
         let options = HIOptions()
-        
-        // Chart type and settings
+
         let chart = HIChart()
         chart.type = "arearange"
         chart.zooming = HIZooming()
         chart.zooming.type = "x"
         chart.scrollablePlotArea = nil
         options.chart = chart
-        
-        // Chart title
+
         let title = HITitle()
         title.text = "Temperature Ranges (Min, Max)"
         options.title = title
-        
-        // X-Axis configuration
+
         let xAxis = HIXAxis()
         xAxis.type = "datetime"
         let accessibility = HIAccessibility()
-        accessibility.rangeDescription = "Date range of the weather data."
+        accessibility.rangeDescription = "Date range"
         xAxis.accessibility = accessibility
         options.xAxis = [xAxis]
-        
-        // Y-Axis configuration
+
         let yAxis = HIYAxis()
         yAxis.title = HITitle()
         yAxis.title.text = "Temperature (°F)"
         options.yAxis = [yAxis]
         
-        // Tooltip configuration
         let tooltip = HITooltip()
         tooltip.shared = NSNumber(value: true)
         tooltip.valueSuffix = "°F"
         tooltip.xDateFormat = "%A, %b %e"
         options.tooltip = tooltip
-        
-        // Disable legend
+
         let legend = HILegend()
         legend.enabled = false
         options.legend = legend
-        
-        // Series configuration
+
         let series = HIArearange()
         series.name = ""
         series.data = temperatureData
         series.color = HIColor()
         options.series = [series]
         
-        // Assign options to the chart view
+        let marker = HIMarker()
+        marker.enabled = true
+        marker.fillColor = HIColor(uiColor: UIColor.black)
+        series.marker = marker
+        
+        let gradientColor = HIColor(linearGradient: ["x1": 0,"x2": 0,"y1": 0,"y2": 1], stops: [[0.0, "rgba(253, 215, 118, 1)"],[1.0, "rgba(156, 197, 255 , 1)"]])
+        series.color = gradientColor
+
         chartView.options = options
         
         chartView.translatesAutoresizingMaskIntoConstraints = false
 
         chartSub.addSubview(chartView)
 
-        // Set constraints to make the chartView fit the container view
         NSLayoutConstraint.activate([
             chartView.leadingAnchor.constraint(equalTo: chartSub.leadingAnchor),
             chartView.trailingAnchor.constraint(equalTo: chartSub.trailingAnchor),
             chartView.topAnchor.constraint(equalTo: chartSub.topAnchor),
             chartView.bottomAnchor.constraint(equalTo: chartSub.bottomAnchor)
         ])
-        
-        // Add chartView to the view
+
         chartSub.addSubview(chartView)
     }
     
